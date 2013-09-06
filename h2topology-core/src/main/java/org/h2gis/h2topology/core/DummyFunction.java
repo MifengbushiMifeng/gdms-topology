260024496c0eb133cd0a54f69451b789b8f83dd2
Adam Gouge <adam.gouge@ec-nantes.fr>
2013-09-06 11:12:26 +0200

    Prepare ST_Graph transformation

--- h2topology-core/src/main/java/org/h2gis/h2topology/core/DummyFunction.java
+++ h2topology-core/src/main/java/org/h2gis/h2topology/core/DummyFunction.java
@@ -5,6 +5,11 @@ import org.h2gis.h2spatialapi.DeterministicScalarFunction;
 
 /**
  * Function sample. Return the area of the parameter.
+ *
+ * To test this in the H2 web interface, do the following: <code>
+ * CREATE ALIAS IF NOT EXISTS MY_FN FOR "org.h2gis.h2topology.core.DummyFunction.test";
+ * CALL MY_FN('POLYGON((0 0,10 0,10 10,0 10,0 0))');</code>
+ * The answer is 100.
  */
 public class DummyFunction extends DeterministicScalarFunction {
     /**
